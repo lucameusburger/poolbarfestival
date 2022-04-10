e725a8e08da84bf49b441c2b45225ff294203f3d
Sakuk <lukas.fritsch30@gmail.com>
2022-04-10 15:21:32 +0200

    refactored

--- modules/FadeInView.js
+++ modules/FadeInView.js
@@ -1,25 +1,29 @@
 import React, { useRef, useEffect } from 'react';
 import { Animated } from 'react-native';
 
-const FadeInView = (props) => {
+const FadeInView = ({ children, style, duration = 1000 }) => {
   const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
 
   useEffect(() => {
     Animated.timing(fadeAnim, {
       toValue: 1,
-      duration: props.duration || 1000,
+      duration: duration,
       useNativeDriver: true,
     }).start();
   }, [fadeAnim]);
 
   return (
     <Animated.View // Special animatable View
-      style={{
-        ...props.style,
-        opacity: fadeAnim, // Bind opacity to animated value
-      }}
+      style={
+        [
+          style,
+          {
+            opacity: fadeAnim, // Bind opacity to animated value
+          }
+        ]
+      }
     >
-      {props.children}
+      {children}
     </Animated.View>
   );
 };
