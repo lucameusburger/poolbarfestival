262677c54f9c58287f96dab2f06950e4ffb6f1c7
Sakuk <lukas.fritsch30@gmail.com>
2022-04-13 14:23:57 +0200

    added heart animation

--- src/components/ui/LikeIcon.js
+++ src/components/ui/LikeIcon.js
@@ -1,13 +1,18 @@
-import { FontAwesome } from '@expo/vector-icons';
-import React, { useState, useEffect } from 'react';
-import { View, Button, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { Animated, TouchableOpacity } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { addCallenderEvent, deleteCallenderEvent } from '../../redux/callenderThunk';
+import { useEffect, useRef } from 'react';
+
+import LottieView from 'lottie-react-native';
+
+import heardfade from '../../../assets/animations/heartfade.json';
 
 TouchableOpacity.defaultProps = { activeOpacity: 0.8 };
 
-const LikeIcon = ({ eventId, color = "#2ECDA7", style }) => {
+const LikeIcon = ({ eventId, style }) => {
     const dispatch = useDispatch();
+    const progress = useRef(new Animated.Value(isLiked ? 1 : 0)).current;
+
     const likeEvent = (id) => {
         dispatch({
             type: 'ADD_TO_LIKED_EVENTS',
@@ -29,29 +34,76 @@ const LikeIcon = ({ eventId, color = "#2ECDA7", style }) => {
 
     const isLiked = likedEvents.includes(eventId);
 
+    const animationDuration = 1000;
+    useEffect(() => {
+        if (isLiked) {
+            Animated.timing(progress, {
+                toValue: 1,
+                duration: animationDuration,
+                useNativeDriver: true,
+            }).start();
+        } else {
+            Animated.timing(progress, {
+                toValue: 0,
+                duration: animationDuration,
+                useNativeDriver: true,
+            }).start();
+        }
+    }, [isLiked]);
 
     return (
-        <FontAwesome
-            style={[
-                {
-                    alignSelf: 'flex-end',
-                    marginBottom: 'auto',
-                    marginTop: 'auto'
-                },
-                style
-            ]}
-            name={isLiked ? 'heart' : 'heart-o'}
-            size={32}
-            color={color}
-            onPress={() => {
-                if (isLiked) {
-                    unLikeEvent(eventId);
-                } else {
-                    likeEvent(eventId);
-                }
-            }}
-        />
+        <>
+            <TouchableOpacity
+                onPress={() => {
+                    if (isLiked) {
+                        unLikeEvent(eventId);
+                    } else {
+                        likeEvent(eventId);
+                    }
+                }}
+
+                style={[
+                    {
+                        alignSelf: 'flex-end',
+                        marginBottom: 'auto',
+                        marginTop: 'auto'
+                    },
+                    style
+
+                ]}
+            >
+                <LottieView
+                    style={{
+                        height: 100,
+                        width: 100,
+                    }}
+                    progress={progress}
+                    source={heardfade}
+                />
+            </TouchableOpacity>
+        </>
     );
 };
-
+/**
+ * <FontAwesome
+                style={[
+                    {
+                        alignSelf: 'flex-end',
+                        marginBottom: 'auto',
+                        marginTop: 'auto'
+                    },
+                    style
+                ]}
+                name={isLiked ? 'heart' : 'heart-o'}
+                size={32}
+                color={color}
+                onPress={() => {
+                    if (isLiked) {
+                        unLikeEvent(eventId);
+                    } else {
+                        likeEvent(eventId);
+                    }
+                }}
+            />
+ */
 export default LikeIcon;
