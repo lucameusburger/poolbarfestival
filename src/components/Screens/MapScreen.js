465c54834aca860977922442cf88dd87f445e432
Felix Kaufmann <patheticcoder@gmail.com>
2022-04-15 13:22:56 +0200

    Fixed MapScreen

--- src/components/Screens/MapScreen.js
+++ src/components/Screens/MapScreen.js
@@ -16,35 +16,67 @@ import geodata from '../../../assets/geodata.json';
 import { FontAwesome } from '@expo/vector-icons';
 
 
-import MapView, {Marker , Callout, Polygon} from 'react-native-maps';
+import MapView, { Marker, Callout, Polygon } from 'react-native-maps';
 import MyCalloutView from '../ui/MyCalloutView';
 
 
 import mapImage from '../../../assets/img/map.png'
 import { fetchSpaceLocations } from '../../redux/spaceLocationThunk';
-
+import markerImage from '../../../assets/img/marker.png';
 
 const BASE_URL = 'https://www.admin.poolbar.at/';
 const mapRef = React.createRef();
 
-const getMarkers = (locations) => {
-    let markers = [];
-    let counter = 0;
-    locations.data.map((location) => markers[counter++] = <Marker key={location.id}
-    image={require('../../../assets/img/marker.png')}
-    coordinate={{'longitude':location.location.coordinates[0],'latitude':location.location.coordinates[1]}}
-    >
-        <Callout tooltip={true} style={{backgroundColor:'transparent'}} onPress={
-                            () =>{
-                                openGoogleMaps({'latitude':location.location.coordinates[1],'longitude':location.location.coordinates[0]},location.name)
-                                console.log('bruh')
-                        }}>
-                <MyCalloutView location={{'latitude':location.location.coordinates[1],'longitude':location.location.coordinates[0]}} name={location.name}></MyCalloutView>
+function CustomMarker({ location, setCurrentLocation }) {
+    return (
+        <Marker
+            key={location.id}
+            image={markerImage}
+            coordinate={{ 'longitude': location.location.coordinates[0], 'latitude': location.location.coordinates[1] }}
+            onPress={() =>
+                setCurrentLocation({ 'longitude': location.location.coordinates[0], 'latitude': location.location.coordinates[1] })
+            }
+        >
+            <Callout
+                tooltip={true}
+                style={{ backgroundColor: 'transparent' }}
+                onPress={
+                    () => {
+                        openGoogleMaps({
+                            'latitude': location.location.coordinates[1],
+                            'longitude': location.location.coordinates[0]
+                        },
+                            location.name
+                        )
+                        console.log('bruh')
+                    }}>
+                <MyCalloutView location={{ 'latitude': location.location.coordinates[1], 'longitude': location.location.coordinates[0] }} name={location.name}></MyCalloutView>
 
 
             </Callout>
-    </Marker>);
-    return markers;
+        </Marker>
+    )
+}
+
+const getMarkers = (locations) => {
+
+    const dispatch = useDispatch();
+
+    const currentLocation = useSelector(state => state.currentLocation);
+
+    const setCurrentLocation = (newLocation) => {
+        dispatch({
+            type: "SET_CURRENTLOCATION",
+            payload: newLocation
+        })
+    }
+
+    return locations.data.map((location) =>
+        <CustomMarker
+            location={location}
+            setCurrentLocation={setCurrentLocation}
+        />
+    );
 }
 
 function openGoogleMaps(location, name) {
@@ -61,7 +93,7 @@ function openGoogleMaps(location, name) {
 
     Linking.openURL(url);
 }
- function setBoundingAustria() {
+function setBoundingAustria() {
     const bboxAustria = [
         {
             latitude: 48.356363,
@@ -75,9 +107,8 @@ function openGoogleMaps(location, name) {
     setBoundingBox(bboxAustria)
 }
 
-// set the bounding box of the map to Austria
- function setBoundingBox(bbox) {
-     console.log('setting box');
+function setBoundingBox(bbox) {
+    console.log('setting box');
     mapRef.current.setMapBoundaries(bbox[0], bbox[1]);
 }
 
@@ -85,225 +116,223 @@ const MapScreen = ({ navigation }) => {
 
 
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const currentLocation = useSelector(state => state.currentLocation);
-
-    const setMode = (CURRENTLOCATION) => {
-        dispatch({
-            type: "SET_CURRENTLOCATION",
-            payload: currentLocation
-        })
-    }
-
-  const locations = useSelector((state) => state.spaceLocations);
-  const isLoaded = useSelector((state) => state.spaceLocations.isLoaded);
-  const isFetchingData = useSelector((state) => state.spaceLocations.isFetchingData);
-  const hasFetchingDataError = useSelector((state) => state.spaceLocations.hasFetchingDataError);
-
-  useEffect(() => {
-    dispatch(fetchSpaceLocations());
-  }, []);
+    const currentLocation = useSelector(state => state.currentLocation);
 
 
-  return (
-    <View style={StylesMain.mainView}>
-      <FadeInView style={{ flex: 1, width: '100%', height: '100%' }}>
-        <NavBar navigation={navigation} title="map" />
-        <MapView
-            minZoomLevel={7}
-            maxZoomLevel={15}
-            style={styles.map} provider = { MapView.PROVIDER_GOOGLE } customMapStyle = { generatedMapStyle }
+    const locations = useSelector((state) => state.spaceLocations);
+    const isLoaded = useSelector((state) => state.spaceLocations.isLoaded);
+    const isFetchingData = useSelector((state) => state.spaceLocations.isFetchingData);
+    const hasFetchingDataError = useSelector((state) => state.spaceLocations.hasFetchingDataError);
 
-            onMapReady={setBoundingAustria}
-            ref={mapRef}
-            initialRegion={initial}>
-          <Polygon
-            coordinates={geodata}
-            strokeWidth={5}
-            strokeColor='black'
-            fillColor='#00000022'
-        />
+    useEffect(() => {
+        dispatch(fetchSpaceLocations());
+    }, []);
 
 
-        {isLoaded&&locations?getMarkers(locations):[]}
+    return (
+        <View style={StylesMain.mainView}>
+            <FadeInView style={{ flex: 1, width: '100%', height: '100%' }}>
+                <NavBar navigation={navigation} title="map" />
+                <MapView
+                    minZoomLevel={7}
+                    maxZoomLevel={15}
+                    style={styles.map}
+                    provider={MapView.PROVIDER_GOOGLE}
+                    customMapStyle={generatedMapStyle}
 
-
-        </MapView>
-        <View style={{position: 'absolute',bottom:0,backgroundColor: 'white', width:'100%',height:'10%', zIndex:10
-            ,display:'flex',flexDirection:'row'}}>
-            <Text>Titel</Text>
-            <Text>Beschreibung</Text>
-            <TouchableOpacity style={{ }} onPress={() => navigation.navigate('Map')}>
-            <FontAwesome style={{ marginTop: 'auto', marginBottom: 'auto', alignSelf: 'center', opacity: 0.6 }} name={'map'} size={36} color="#000" />
-          </TouchableOpacity>
+                    onMapReady={setBoundingAustria}
+                    ref={mapRef}
+                    initialRegion={initial}>
+                    <Polygon
+                        coordinates={geodata}
+                        strokeWidth={5}
+                        strokeColor='black'
+                        fillColor='#00000022'
+                    />
+                    {(isLoaded && locations) ?
+                        getMarkers(locations) :
+                        null
+                    }
+                </MapView>
+                <View style={{
+                    position: 'absolute', bottom: 0, backgroundColor: 'white', width: '100%', height: '10%', zIndex: 10
+                    , display: 'flex', flexDirection: 'row'
+                }}>
+                    <Text>Titel</Text>
+                    <Text>Beschreibung</Text>
+                    <TouchableOpacity style={{}} onPress={() => openGoogleMaps(currentLocation)}>
+                        <FontAwesome style={{ marginTop: 'auto', marginBottom: 'auto', alignSelf: 'center', opacity: 0.6 }} name={'map'} size={36} color="#000" />
+                    </TouchableOpacity>
+                </View>
+            </FadeInView>
         </View>
-      </FadeInView>
-    </View>
 
 
-  );
+    );
 };
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    width: '100%',
-    height: '82%',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    map: {
+        width: '100%',
+        height: '82%',
+    },
 });
 
 const generatedMapStyle = [
-  {
-      "featureType": "water",
-      "elementType": "all",
-      "stylers": [
-          {color: "#666666"
-          },
-          {
-              "visibility": "on"
-          }
-      ]
-  },
-  {
-      "featureType": "landscape",
-      "elementType": "all",
-      "stylers": [
-          {
-              "hue": "#ffffff"
-          },
-          {
-              "saturation": -100
-          },
-          {
-              "lightness": 100
-          },
-          {
-              "visibility": "off"
-          }
-      ]
-  },
-  {
-      "featureType": "road",
-      "elementType": "geometry",
-      "stylers": [
-          {
-              "hue": "#000000"
-          },
-          {
-              "saturation": -100
-          },
-          {
-              "lightness": -100
-          },
-          {
-              "visibility": "simplified"
-          }
-      ]
-  },
-  {
-      "featureType": "road",
-      "elementType": "labels",
-      "stylers": [
-          {
-              "hue": "#ffffff"
-          },
-          {
-              "saturation": -100
-          },
-          {
-              "lightness": 100
-          },
-          {
-              "visibility": "off"
-          }
-      ]
-  },
-  {
-      "featureType": "poi",
-      "elementType": "all",
-      "stylers": [
-          {
-              "hue": "#ffffff"
-          },
-          {
-              "saturation": -100
-          },
-          {
-              "lightness": 100
-          },
-          {
-              "visibility": "off"
-          }
-      ]
-  },
-  {
-      "featureType": "administrative",
-      "elementType": "all",
-      "stylers": [
-          {
-              "hue": "#ffffff"
-          },
-          {
-              "saturation": 0
-          },
-          {
-              "lightness": 100
-          },
-          {
-              "visibility": "off"
-          }
-      ]
-  },
-  {
-      "featureType": "transit",
-      "elementType": "geometry",
-      "stylers": [
-          {
-              "hue": "#000000"
-          },
-          {
-              "saturation": 0
-          },
-          {
-              "lightness": -100
-          },
-          {
-              "visibility": "on"
-          }
-      ]
-  },
-  {
-      "featureType": "transit",
-      "elementType": "labels",
-      "stylers": [
-          {
-              "hue": "#ffffff"
-          },
-          {
-              "saturation": 0
-          },
-          {
-              "lightness": 100
-          },
-          {
-              "visibility": "off"
-          }
-      ]
-  }
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                color: "#666666"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#ffffff"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#000000"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": -100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "hue": "#ffffff"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#ffffff"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#ffffff"
+            },
+            {
+                "saturation": 0
+            },
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#000000"
+            },
+            {
+                "saturation": 0
+            },
+            {
+                "lightness": -100
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "hue": "#ffffff"
+            },
+            {
+                "saturation": 0
+            },
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    }
 ];
 
 const initial = {
-  latitude: 47.36321774000127,
-  longitude: 9.689607548263336,
-  latitudeDelta: 0.01,
-  longitudeDelta: 0.01,
+    latitude: 47.36321774000127,
+    longitude: 9.689607548263336,
+    latitudeDelta: 1,
+    longitudeDelta: 1,
 };
 
 export default MapScreen;
