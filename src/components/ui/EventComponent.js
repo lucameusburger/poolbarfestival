25a173259efcb1e9c8b753ee205421fc764114db
Sakuk <lukas.fritsch30@gmail.com>
2022-04-15 13:00:53 +0200

    dd

--- src/components/ui/EventComponent.js
+++ src/components/ui/EventComponent.js
@@ -4,35 +4,47 @@ import LikeIcon from '../ui/LikeIcon';
 import { navigate } from '../../core/RootNavigation';
 import { getDateString } from '../../core/helpers';
 
+
 const EventComponent = ({ item }) => {
   let dateString = item.day_item.date_start ? getDateString(new Date(item.day_item.date_start)) : 'tba';
 
   return (
     <TouchableOpacity
+      style={{
+        padding: 10,
+        borderBottomWidth: 2,
+        borderBottomColor: '#000',
+        width: '100%',
+
+      }}
+      key={item.id}
       onPress={() =>
         navigate('Event', {
           id: item.id,
         })
       }
-      style={{ borderWidth: 2, borderColor: 'black', borderRadius: 10, margin: 5, padding: 10 }}
     >
-      <View key={item.id} style={{ flex: 1, flexWrap: 'wrap' }}>
-        <View style={{ width: '100%', marginTop: 'auto', flexDirection: 'row', height: '100%' }}>
-          <View style={{ padding: 10, width: '100%' }}>
-            <Text style={StylesMain.eventListDateText}>{dateString}</Text>
-            <Text style={StylesMain.eventListMainText}>{item.name}</Text>
-          </View>
-          <View
-            style={{
-              alignSelf: 'flex-end',
-              width: '10%',
-              height: '100%',
-              justifyContent: 'flex-end',
-              marginLeft: 'auto',
-            }}
-          >
-            <LikeIcon eventId={item.id} color={'#00ff00'} />
-          </View>
+      <View style={{
+        width: '100%',
+        flexDirection: 'row',
+      }}>
+        <View style={{
+
+          flex: 8
+        }}>
+          <Text style={StylesMain.eventListDateText}>{dateString}</Text>
+          <Text style={StylesMain.eventListMainText}>{item.name}</Text>
+        </View>
+        <View style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        >
+          <LikeIcon
+            eventId={item.id}
+
+          />
         </View>
       </View>
     </TouchableOpacity>
