c8378e9a9d9d968dfef14c1b1f59bcfa39771da4
Sakuk <lukas.fritsch30@gmail.com>
2022-04-30 14:02:55 +0200

    prettier

--- src/components/screens/ScanScreen.js
+++ src/components/screens/ScanScreen.js
@@ -1,13 +1,13 @@
-import { useState, useEffect } from 'react';
-import { StyleSheet, View, Linking, Dimensions } from 'react-native';
-import FadeInView from '../ui/FadeInView';
-import NavBar from '../ui/NavBar';
-import StylesMain from '../../../styles/StylesMain';
-import { BarCodeScanner } from 'expo-barcode-scanner';
-import { FontAwesome } from '@expo/vector-icons';
-import { navigate } from '../../core/RootNavigation';
+import { useState, useEffect } from "react";
+import { StyleSheet, View, Linking, Dimensions } from "react-native";
+import FadeInView from "../ui/FadeInView";
+import NavBar from "../ui/NavBar";
+import StylesMain from "../../../styles/StylesMain";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { FontAwesome } from "@expo/vector-icons";
+import { navigate } from "../../core/RootNavigation";
 
-const { width: SCREEN_WIDTH } = Dimensions.get('window');
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 const ScanScreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
@@ -18,7 +18,7 @@ const ScanScreen = ({ navigation }) => {
       const { granted } = await BarCodeScanner.getPermissionsAsync();
       if (!granted) {
         const { status } = await BarCodeScanner.requestPermissionsAsync();
-        setHasPermission(status === 'granted');
+        setHasPermission(status === "granted");
       } else {
         setHasPermission(true);
       }
@@ -29,8 +29,8 @@ const ScanScreen = ({ navigation }) => {
     setScanned(true);
     const canOpen = await Linking.canOpenURL(data);
     if (canOpen) {
+      console.log(data);
       await Linking.openURL(data);
-      //timeout 3s
       setTimeout(() => {
         setScanned(false);
       }, 1000);
@@ -39,42 +39,47 @@ const ScanScreen = ({ navigation }) => {
 
   return (
     <View style={StylesMain.mainView}>
-      <FadeInView style={{ flex: 1, width: '100%', height: '100%' }}>
+      <FadeInView style={{ flex: 1, width: "100%", height: "100%" }}>
         <NavBar
           navigation={navigation}
           title="scan"
           next={() => {
-            navigate('ScanCollection');
+            navigate("ScanCollection");
           }}
-          nextTitle={'sammlung'}
+          nextTitle={"sammlung"}
         />
         <View style={{ flex: 1 }}>
           <View
             style={{
-              position: 'absolute',
+              position: "absolute",
               left: 0,
               right: 0,
               bottom: 0,
               right: 0,
               zIndex: 99,
-              width: '100%',
-              height: '100%',
-              alignItems: 'center',
+              width: "100%",
+              height: "100%",
+              alignItems: "center",
             }}
           >
             <FontAwesome
               style={{
-                marginTop: 'auto',
-                marginBottom: 'auto',
+                marginTop: "auto",
+                marginBottom: "auto",
                 zIndex: 99,
                 opacity: 0.33,
               }}
-              name={'qrcode'}
+              name={"qrcode"}
               size={SCREEN_WIDTH}
               color="white"
             />
           </View>
-          {hasPermission && <BarCodeScanner onBarCodeScanned={scanned ? undefined : handleBarCodeScanned} style={[StyleSheet.absoluteFillObject, { margin: -20 }]} />}
+          {hasPermission && (
+            <BarCodeScanner
+              onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+              style={[StyleSheet.absoluteFillObject, { margin: -20 }]}
+            />
+          )}
         </View>
       </FadeInView>
     </View>
