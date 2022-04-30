f50a75bfec975c00a5ef60d3b952853a111ded5e
lucameusburger <meusburger.luca@gmail.com>
2022-04-30 14:53:39 +0200

    display scans

--- src/components/screens/ScanCollectionScreen.js
+++ src/components/screens/ScanCollectionScreen.js
@@ -18,9 +18,7 @@ import { FontAwesome } from '@expo/vector-icons';
 
 const BASE_URL = 'https://www.admin.poolbar.at/';
 
-const ScanCollection = ({ collection }) => {
-  const video = useRef('../../../assets/video/ladder.mp4');
-
+const ScanCollection = ({ collection, events, artists, generators }) => {
   return collection && collection.length > 0 ? (
     collection &&
       collection.length &&
@@ -52,8 +50,8 @@ const ScanCollection = ({ collection }) => {
                   width: '100%',
                 }}
               >
-                <Text style={StylesMain.artistListDateText}>{item.collection}</Text>
-                <Text style={StylesMain.artistListMainText}>{item.name}</Text>
+                <Text style={StylesMain.artistListDateText}>{item.type}</Text>
+                <Text style={StylesMain.artistListMainText}>{item.type === 'event' ? events.find((event) => event.id === item.id).name : item.type === 'artist' ? artists.find((artist) => artist.id === item.id).name : item.type === 'generator_project' ? generators.find((generator) => generator.id === item.id).name : 'unknown'}</Text>
               </View>
             </View>
           </View>
@@ -67,9 +65,15 @@ const ScanCollection = ({ collection }) => {
 const ScanCollectionScreen = ({}) => {
   const dispatch = useDispatch();
 
-  const [collection, setCollection] = useState([{ id: 'asjndajsklndaksn', name: 'Stagedesign', collection: 'generator_projects' }]);
+  const scans = useSelector((state) => state.scanns.data);
+  const events = useSelector((state) => state.events.data);
+  const artists = useSelector((state) => state.artists.artists);
+  const generators = useSelector((state) => state.generators.data);
 
+  const [selectedScans, setSelectedScans] = useState([]);
   const isLoaded = useSelector((state) => state.artists.isLoaded);
+
+  console.log(scans);
 
   return (
     <View style={StylesMain.mainView}>
@@ -78,7 +82,7 @@ const ScanCollectionScreen = ({}) => {
         <View style={{ padding: 10, borderBottomColor: '#000000', borderBottomWidth: 2 }}>
           <ProgressBar value={66} />
         </View>
-        <ScrollView style={{ flex: 1 }}>{!isLoaded ? <LoadingText /> : <ScanCollection collection={collection} />}</ScrollView>
+        <ScrollView style={{ flex: 1 }}>{!isLoaded || !scans || !artists || !events || !generators ? <LoadingText /> : <ScanCollection events={events} collection={scans} generators={generators} artists={artists} />}</ScrollView>
       </FadeInView>
     </View>
   );
