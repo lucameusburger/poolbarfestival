3c12897f3661ef8e5049cc5cd9e260d603637de3
Sakuk <lukas.fritsch30@gmail.com>
2022-04-30 14:21:17 +0200

    refactor

--- src/redux/poiThunk.js
+++ src/redux/poiThunk.js
@@ -1,22 +1,22 @@
-const BASE_URL = 'https://www.admin.poolbar.at/';
+const BASE_URL = "https://www.admin.poolbar.at/";
 
 function setPOI(poi) {
   return {
-    type: 'SET_POI',
+    type: "SET_POI",
     payload: poi,
   };
 }
 
 function setIsFetchingData(isFetching) {
   return {
-    type: 'SET_IS_FETCHING_POI',
+    type: "SET_IS_FETCHING_POI",
     payload: isFetching,
   };
 }
 
 function setHasFetchingDataError(hasError) {
   return {
-    type: 'SET_HAS_FETCHING_POI_ERROR',
+    type: "SET_HAS_FETCHING_POI_ERROR",
     payload: hasError,
   };
 }
@@ -26,9 +26,9 @@ export function fetchPOI() {
     if (isFetchingData) {
       return;
     } else {
-      console.log('fetchPOI');
+      console.log("fetchPOI");
       dispatch(setIsFetchingData(true));
-      fetch(BASE_URL + 'items/points_of_interest')
+      fetch(BASE_URL + "items/points_of_interest")
         .then(async (response) => {
           // check for error response
           console.log(response.ok);
@@ -36,21 +36,20 @@ export function fetchPOI() {
           if (response.ok) {
             const data = await response.json();
             let fetchedPOI = data.data || [];
-            console.log('pois', fetchedPOI);
 
             dispatch(setPOI(fetchedPOI));
           } else {
             const error = (data && data.message) || response.status;
-            console.log('Fetching POI Error: ', error);
+            console.log("Fetching POI Error: ", error);
             dispatch(setHasFetchingDataError(true));
           }
         })
         .catch((error) => {
-          console.error('Error loading POI: ', error);
+          console.error("Error loading POI: ", error);
           dispatch(setHasFetchingDataError(true));
         })
         .finally(() => {
-          console.log('Finished loading POI');
+          console.log("Finished loading POI");
           dispatch(setIsFetchingData(false));
         });
     }
