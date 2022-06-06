6678fd3d2aad88e696854ad0ef0d78d00eb18d63
Sakuk <lukas.fritsch30@gmail.com>
2022-06-07 01:16:22 +0200

    made max Value realy max

--- src/components/ui/ProgressBar.js
+++ src/components/ui/ProgressBar.js
@@ -2,8 +2,26 @@ import { Text, View } from 'react-native';
 
 const ProgressBar = ({ value, maxvalue, text }) => {
   return (
-    <View style={{ width: '100%', borderBottomColor: '#000000', borderWidth: 2, minHeight: 10 }}>
-      <View style={{ backgroundColor: '#00ff00', width: (100 / maxvalue) * value + '%', alignItems: 'center', minHeight: 6 }}>{!!text && <Text style={{ marginVertical: 5, fontSize: 52 }}>{text}</Text>}</View>
+    <View
+      style={{
+        width: '100%',
+        borderBottomColor: '#000000',
+        borderWidth: 2,
+        minHeight: 10,
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: '#00ff00',
+          width: Math.min(maxvalue, (100 / maxvalue) * value) + '%',
+          alignItems: 'center',
+          minHeight: 6,
+        }}
+      >
+        {!!text && (
+          <Text style={{ marginVertical: 5, fontSize: 52 }}>{text}</Text>
+        )}
+      </View>
     </View>
   );
 };
