aeef3811d8cf1037bf137ec07d45c6851019f40f
Sakuk <lukas.fritsch30@gmail.com>
2022-07-12 15:56:59 +0200

    Added Weekday to date

--- src/core/helpers.js
+++ src/core/helpers.js
@@ -1,33 +1,41 @@
-import { Linking, Platform } from "react-native";
+import { Linking, Platform } from 'react-native';
 
 export function getDateString(date) {
   const monthName = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
   ];
 
+  const weekdayName = ['MO', 'DI', 'MI', 'DO', 'FR', 'SA', 'SO'];
+
   return (
-    date.getDate() + " " + monthName[date.getMonth()] + " " + date.getFullYear()
+    weekdayName[date.getDay()] +
+    ' ' +
+    date.getDate() +
+    ' ' +
+    monthName[date.getMonth()] +
+    ' ' +
+    date.getFullYear()
   );
 }
 
 export function openGoogleMaps(location, name) {
   const scheme = Platform.select({
-    ios: "maps:0,0?q=",
-    android: "geo:0,0?q=",
+    ios: 'maps:0,0?q=',
+    android: 'geo:0,0?q=',
   });
   const latLng = `${location.latitude},${location.longitude}`;
-  const label = "Custom Label";
+  const label = 'Custom Label';
   const url = Platform.select({
     ios: `${scheme}${name}@${latLng}`,
     android: `${scheme}${latLng}(${name})`,
@@ -37,6 +45,6 @@ export function openGoogleMaps(location, name) {
 }
 
 export function randomId(len) {
-  var p = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  return [...Array(len)].reduce((a) => a + p[~~(Math.random() * p.length)], "");
+  var p = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  return [...Array(len)].reduce((a) => a + p[~~(Math.random() * p.length)], '');
 }
